ea3ac278e0e3c1b4cb11c4561206f91495c78d88
Jorge coot <43214398+Fairytail36@users.noreply.github.com>
2019-03-06 09:06:01 -0800

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,23 @@
+//Spell #1
+
+$(".one-piece").click(function(){
+ $(".1").fadeToggle();
+});
+
+
+
+//Spell #2
+
+$(".My-hero-academia").click(function(){
+ $(".2").slideToggle();
+});
+
+
+
+//Spell #3
+
+$(".hunter-x-hunter").click(function(){
+ $(".3").hide();
+});
+
+
